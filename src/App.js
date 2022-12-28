87b1da89e35c0201f0ea48a649b1dd95730f65f3
aziz-codes <azizisoffline@gmail.com>
2022-12-29 00:17:22 +0500

    done with next and previous

--- src/App.js
+++ src/App.js
@@ -5,39 +5,47 @@ import { useState, useEffect } from "react";
 const App = () => {
   const [users, setUsers] = useState([]);
   const [index, setIndex] = useState(0)
-
+const [loading, setloading] = useState(false);
   const fetchUsers = () => {
+    setloading(true);
     getUsers().then((res) => {
       const {data} = res.data;
        setUsers(data);
        console.log(data)
     });
-   
+   setloading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
+  let usersLength = users.length-1;
   
   const handleNext = () =>{
-   
+   setIndex((currentIndex)=>currentIndex+1);
+   if(index===usersLength){
+    setIndex(0);
+   }
   }
 
- 
+  const handlePrevious = () =>{
+    setIndex((currentIndex)=>currentIndex-1);
+   }
+//  console.log(users[0].id);
 
 
   return (
   <div className="flex  w-full justify-center items-center  ">
     {/* <img src={users[0].image}/> */}
-    <div className="flex flex-col rounded-md justify-center items-center border mt-5 h-56 w-56">
-      {/* <img src={users[index].avatar} className="h-20 w-20 rounded-full border p-2 " alt="profile"/> */}
-      {/* <h4>{users[0].first_name}</h4> */}
+   {loading ? "Loading" :  <div className="flex flex-col rounded-md justify-center items-center border mt-5 h-56 w-56">
+      <img src={users[index]?.avatar} className="h-20 w-20 rounded-full border p-2 " alt="profile"/>
+      <h4>{users[index]?.id}</h4>
       <div className="flex flex-row gap-3 mt-3">
         <button className="rounded-sm p-1 text-white bg-sky-500" onClick={handleNext}>Next</button>
-        <button className="rounded-sm p-1 text-white bg-sky-500">Previous</button>
+        <button className="rounded-sm p-1 text-white bg-sky-500" onClick={handlePrevious}>Previous</button>
       </div>
-    </div>
+    </div>}
   </div>
   );
 };
